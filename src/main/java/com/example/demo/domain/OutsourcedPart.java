8e38dab93a5f0a83009136936ae1f9065c317009
Christian Hogan <choga83@wgu.edu>
2024-06-09 12:27:57 +0300

    Added constructors to InhousePart and OutsourcedPart. Updated DemoApplication with sample inventory.

--- src/main/java/com/example/demo/domain/OutsourcedPart.java
+++ src/main/java/com/example/demo/domain/OutsourcedPart.java
@@ -3,18 +3,17 @@ package com.example.demo.domain;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
-/**
- *
- *
- *
- *
- */
 @Entity
 @DiscriminatorValue("2")
-public class OutsourcedPart extends Part{
-String companyName;
+public class OutsourcedPart extends Part {
+    String companyName;
 
     public OutsourcedPart() {
+    }
+
+    public OutsourcedPart(String name, double price, int inv, String companyName) {
+        super(name, price, inv);
+        this.companyName = companyName;
     }
 
     public String getCompanyName() {
